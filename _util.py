5256cddb137dad4fc7b23b8ca3c579e65c5ebccc
Eric Le Lay <elelay@macports.org>
2020-02-02 14:26:59 +0100

    working gpodder ppas
    warning: I published 3.10.13 stable with too new debhelper at first, so not available for bionic

---
+++ _util.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import subprocess
+import sys
+import os
+
+def parse_args():
+    parser = OptionParser()
+    parser.add_option("-r", "--release", dest="release", action="store_true",
+                      default=False, help="stable release")
+
+    parser.add_option("-d", "--dist", action="store", dest="dist",
+                      default="ubuntu", help="ubuntu/debian",
+                      choices=["ubuntu", "debian"])
+
+    parser.add_option("-v", "--version", action="store", dest="version",
+                      default=0, type="int", help="version increment")
+
+    return parser.parse_args()[0]
+
+def cd(d):
+    old = os.getcwd()
+    os.chdir(d)
+    print("> CD: %s -> %s" % (old, d))
+
+def p(cmd):
+    print("> %s" % cmd)
+    pipe = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell=True, stdout=pipe, stderr=pipe, stdin=pipe, encoding="utf-8")
+    stdout, stderr = p.communicate()
+    return p.returncode, stdout.strip(), stderr.strip()
+
+def clean(directory, *packages):
+    old = os.getcwd()
+    os.chdir(directory)
+    for package in packages:
+        cmd = "rm %s*.changes %s*.tar.gz %s*.tar.xz %s*.dsc %s*.upload %s*.deb %s*.build %s*.buildinfo %s*.diff.gz" % ((package,) * 9)
+        p(cmd)
+    os.chdir(old)
+
+
+def failed(out):
+    status, stdout, stderr = out
+    return status != 0
+
+
+def fail(out):
+    status, stdout, stderr = out
+    if status != 0:
+        print("#" * 24)
+        print(stdout)
+        print(stderr)
+        print("#" * 24)
+        sys.exit()
+    return out
---
+++ gpodder.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import os
+from _util import *
+
+##########################################################
+
+PACKAGE= "gpodder"
+PPA_VERSION = "3.10.13.99"
+RELEASE_VERSION = "3.10.13"
+
+##########################################################
+
+args = parse_args()
+
+if args.dist == "ubuntu":
+    dput_cfg = os.path.join(os.getcwd(), "dput.cf")
+else:
+    raise Exception("Debian repo unsupported")
+    dput_cfg = os.path.join(os.getcwd(), "dput_debian.cf")
+
+start_dir = os.getcwd()
+clean(start_dir, PACKAGE)
+
+git_dir = "gpodder-git"
+if not os.path.isdir(git_dir):
+    p("git clone https://github.com/gpodder/gpodder.git %s" % git_dir)
+cd(git_dir)
+
+
+p("git reset HEAD --hard")
+p("git clean -xfd")
+p("git checkout master")
+p("git pull --all")
+if args.release:
+    fail(p("git checkout %s" % RELEASE_VERSION))
+
+rev_num = p("git rev-list --count HEAD")[1]
+rev_hash = p("git rev-parse --short HEAD")[1]
+rev = rev_num  +"~" + rev_hash
+date = p("date -R")[1]
+
+if not args.release:
+   p("echo 'BUILD_INFO = u\"%s\"' >> 'src/gpodder/build_info.py'" % rev_hash)
+
+if not args.release:
+    UPSTREAM_VERSION = PPA_VERSION + "+" + rev_num + "~" + rev_hash
+else:
+    UPSTREAM_VERSION = RELEASE_VERSION
+if args.version != 0:
+    UPSTREAM_VERSION += "+%s" % args.version
+p("tar -pczf ../%s_%s.orig.tar.gz --exclude .git --strip-components=1 --transform='s,^./,gpodder/,' %s" % (PACKAGE, UPSTREAM_VERSION, "."))
+
+if args.release:
+    debian_dir = "debian_gpodder_stable"
+else:
+    debian_dir = "debian_gpodder"
+
+if args.dist == "debian":
+    if args.release:
+        releases = {"gpodder-stable": debian_dir}
+    else:
+        releases = {"gpodder-unstable": debian_dir}
+else:
+    releases = {
+        "bionic": debian_dir,
+        "disco": debian_dir,
+        "eoan": debian_dir,
+    }
+
+for release, debian_dir in releases.items():
+    p("rm -R debian")
+    p("cp -R ../%s ." % debian_dir)
+    p("mv %s debian" % debian_dir)
+
+    debian_version = "%s-0~ppa%s~%s" % (UPSTREAM_VERSION, args.version, release.replace("-", "~"))
+
+    changelog = "debian/changelog"
+    t = open(changelog).read()
+    t = t.replace("%version%", debian_version)
+    t = t.replace("%dist%", release)
+    t = t.replace("%date%", date)
+    with open(changelog, "w") as h:
+        h.write(t)
+
+    if args.dist == "debian":
+        if args.release:
+            fail(p("pdebuild --use-pdebuild-internal --debbuildopts '-uc -us' --buildresult .."))
+        else:
+            fail(p("dpkg-buildpackage -uc -us -tc -I -rfakeroot"))
+    else:
+        fail(p("dpkg-buildpackage -uc -us -S -tc -I -rfakeroot"))
+
+p("rm -R debian")
+
+p("echo debsign -k '9FCE 0930 7C7D A5B0 6E3C AC79 409C B5A2 2813 3CC6' %s*.changes" % (PACKAGE,))
+dput = "dput --config '%s'" % dput_cfg
+if args.dist == "debian":
+    fail(p("%s local %s*.changes" % (dput, PACKAGE)))
+else:
+    if args.release:
+        fail(p("echo %s stable %s*.changes" % (dput, PACKAGE)))
+    else:
+        fail(p("echo %s unstable %s*.changes" % (dput, PACKAGE)))
+    # fail(p("%s experimental %s*.changes" % (dput, PACKAGE)))
+
+# clean(start_dir, PACKAGE)
